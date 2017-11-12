9e6f282279e0b5746cfe775911be37f755346cb5
mikemajesty <mikemajesty2011@gmail.com>
2017-11-12 20:22:15 -0200

    add dist server

--- dist/server.js
+++ dist/server.js
@@ -30,10 +30,15 @@ var _base_routes = require('./services/base_routes');
 
 var _base_routes2 = _interopRequireDefault(_base_routes);
 
+var _cors = require('cors');
+
+var _cors2 = _interopRequireDefault(_cors);
+
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
 const app = (0, _express2.default)();
 
+app.use((0, _cors2.default)());
 app.use(_bodyParser2.default.json());
 
 const knex = process.env.ENV === 'production' ? new _knex2.default(_knexfile2.default.production) : new _knex2.default(_knexfile2.default.development);
